36fbd5ac4604ece35fc2e3a4ed79d87aa41d2d7d
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-28 15:47:40 +0700

    add prefetcher classes

---
+++ classes/prefetchers.py
@@ -0,0 +1,76 @@
+import torch
+from torch.utils.data import DataLoader
+
+
+__all__ = ["CPUPrefetcher", "CUDAPrefetcher"]
+
+
+class CPUPrefetcher:
+    """Use the CPU side to accelerate data reading.
+
+    Args:
+        dataloader (DataLoader): Data loader. Combines a dataset and a sampler,
+            and provides an iterable over the given dataset.
+    """
+
+    def __init__(self, dataloader: DataLoader) -> None:
+        self.original_dataloader = dataloader
+        self.data = iter(dataloader)
+
+    def next(self):
+        try:
+            return next(self.data)
+        except StopIteration:
+            return None
+
+    def reset(self):
+        self.data = iter(self.original_dataloader)
+
+    def __len__(self) -> int:
+        return len(self.original_dataloader)
+
+
+class CUDAPrefetcher:
+    """Use the CUDA side to accelerate data reading.
+
+    Args:
+        dataloader (DataLoader): Data loader. Combines a dataset and a sampler,
+            and provides an iterable over the given dataset.
+        device (torch.device): Specify running device.
+    """
+
+    def __init__(self, dataloader: DataLoader, device: torch.device):
+        self.batch_data = None
+        self.original_dataloader = dataloader
+        self.device = device
+
+        self.data = iter(dataloader)
+        self.stream = torch.cuda.Stream()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.batch_data = next(self.data)
+        except StopIteration:
+            self.batch_data = None
+            return None
+
+        with torch.cuda.stream(self.stream):
+            for k, v in self.batch_data.items():
+                if torch.is_tensor(v):
+                    self.batch_data[k] = self.batch_data[k].to(
+                        self.device, non_blocking=True
+                    )
+
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        batch_data = self.batch_data
+        self.preload()
+        return batch_data
+
+    def reset(self):
+        self.data = iter(self.original_dataloader)
+        self.preload()
+
+    def __len__(self) -> int:
+        return len(self.original_dataloader)
