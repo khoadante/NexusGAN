a7daac0bd0a42af1cb33d75991e1c9254bb04035
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-29 01:58:22 +0700

    hotfix sunhays80 download url

--- scripts/download_sunhays80.py
+++ scripts/download_sunhays80.py
@@ -1,10 +1,7 @@
 import os
 import zipfile
 
-os.system("wget -N https://dl.dropboxusercontent.com/s/dm1uazkyprejeyf/SunHays80.zip?dl=0")
-
-if not os.path.exists("datasets/SunHays80"):
-    os.makedirs("datasets/SunHays80")
+os.system("wget -N https://dl.dropboxusercontent.com/s/dm1uazkyprejeyf/SunHays80.zip")
 
 with zipfile.ZipFile("SunHays80.zip", "r") as zip_ref:
-    zip_ref.extractall("datasets/SunHays80")
+    zip_ref.extractall("datasets")
