e34b779797ed31da23260b12857fb17f864ead2b
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-28 15:50:42 +0700

    add realesrgan blocks

---
+++ networks/realesrgan/blocks.py
@@ -0,0 +1,72 @@
+import torch
+from torch import nn
+
+__all__ = ["ResidualDenseBlock", "ResidualResidualDenseBlock"]
+
+
+class ResidualDenseBlock(nn.Module):
+    """Achieves densely connected convolutional layers.
+    `Densely Connected Convolutional Networks <https://arxiv.org/pdf/1608.06993v5.pdf>` paper.
+    Args:
+        channels (int): The number of channels in the input image.
+        growth_channels (int): The number of channels that increase in each layer of convolution.
+    """
+
+    def __init__(self, channels: int, growth_channels: int) -> None:
+        super(ResidualDenseBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            channels + growth_channels * 0, growth_channels, (3, 3), (1, 1), (1, 1)
+        )
+        self.conv2 = nn.Conv2d(
+            channels + growth_channels * 1, growth_channels, (3, 3), (1, 1), (1, 1)
+        )
+        self.conv3 = nn.Conv2d(
+            channels + growth_channels * 2, growth_channels, (3, 3), (1, 1), (1, 1)
+        )
+        self.conv4 = nn.Conv2d(
+            channels + growth_channels * 3, growth_channels, (3, 3), (1, 1), (1, 1)
+        )
+        self.conv5 = nn.Conv2d(
+            channels + growth_channels * 4, channels, (3, 3), (1, 1), (1, 1)
+        )
+
+        self.leaky_relu = nn.LeakyReLU(0.2, True)
+        self.identity = nn.Identity()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out1 = self.leaky_relu(self.conv1(x))
+        out2 = self.leaky_relu(self.conv2(torch.cat([x, out1], 1)))
+        out3 = self.leaky_relu(self.conv3(torch.cat([x, out1, out2], 1)))
+        out4 = self.leaky_relu(self.conv4(torch.cat([x, out1, out2, out3], 1)))
+        out5 = self.identity(self.conv5(torch.cat([x, out1, out2, out3, out4], 1)))
+        out = torch.mul(out5, 0.2)
+        out = torch.add(out, identity)
+
+        return out
+
+
+class ResidualResidualDenseBlock(nn.Module):
+    """Multi-layer residual dense convolution block.
+    Args:
+        channels (int): The number of channels in the input image.
+        growth_channels (int): The number of channels that increase in each layer of convolution.
+    """
+
+    def __init__(self, channels: int, growth_channels: int) -> None:
+        super(ResidualResidualDenseBlock, self).__init__()
+        self.rdb1 = ResidualDenseBlock(channels, growth_channels)
+        self.rdb2 = ResidualDenseBlock(channels, growth_channels)
+        self.rdb3 = ResidualDenseBlock(channels, growth_channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.rdb1(x)
+        out = self.rdb2(out)
+        out = self.rdb3(out)
+        out = torch.mul(out, 0.2)
+        out = torch.add(out, identity)
+
+        return out
