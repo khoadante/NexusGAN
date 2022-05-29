f9dc0f114e8156839c974a1a9ccc6e4768ed68cc
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-29 09:15:37 +0700

    update training settings

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,14 +1,7 @@
 import os
 import zipfile
 
-os.system("wget -N http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip")
-os.system("wget -N http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip")
+os.system("wget -N https://dl.dropboxusercontent.com/s/dm1uazkyprejeyf/SunHays80.zip")
 
-if not os.path.exists("datasets/DIV2K"):
-    os.makedirs("datasets/DIV2K")
-
-with zipfile.ZipFile("DIV2K_train_HR.zip", "r") as zip_ref:
-    zip_ref.extractall("datasets/DIV2K")
-
-with zipfile.ZipFile("DIV2K_valid_HR.zip", "r") as zip_ref:
-    zip_ref.extractall("datasets/DIV2K")
+with zipfile.ZipFile("SunHays80.zip", "r") as zip_ref:
+    zip_ref.extractall("datasets")
--- scripts/download_sunhays80.py
+++
@@ -1,7 +0,0 @@
-import os
-import zipfile
-
-os.system("wget -N https://dl.dropboxusercontent.com/s/dm1uazkyprejeyf/SunHays80.zip")
-
-with zipfile.ZipFile("SunHays80.zip", "r") as zip_ref:
-    zip_ref.extractall("datasets")
